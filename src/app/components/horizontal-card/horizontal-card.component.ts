0eeaba88fecd5f9b86a35416b6ed5a8b708ddb54
aaya-ahmed <aaya7098@gmail.com>
2024-12-06 13:38:44 +0200

    update horizontal component

--- src/app/components/horizontal-card/horizontal-card.component.ts
+++ src/app/components/horizontal-card/horizontal-card.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Tcard } from 'src/app/models/card';
+import { Component, Input } from '@angular/core';
+import { Tcard } from '../../models/card';
+import {  Router } from '@angular/router';
+import { LoadingService } from 'src/app/services/shared/loading.service';
+
 
 @Component({
   selector: 'app-horizontal-card',
@@ -13,4 +16,17 @@ export class HorizontalCardComponent {
     body: '',
     image: ''
   };
+  @Input()goBackUrl:string='';
+  constructor(
+    private router: Router,
+    private loadingService: LoadingService,
+  ) {
+  }
+  goBack(): void {
+    if(this.goBackUrl){
+
+      this.loadingService.loading.next(true);
+      this.router.navigateByUrl(this.goBackUrl);
+    }
+  }
 }
